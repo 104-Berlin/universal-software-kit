065b83d208d9306a3056beeed4cd1d924f792a0b
J4m3s00 <james.lol2000@gmail.com>
2021-04-14 16:56:18 +0200

    Update prefix_shared.h

--- src/shared/prefix_shared.h
+++ src/shared/prefix_shared.h
@@ -1,16 +1,5 @@
 #pragma once
 
-#include "glm/glm/glm.hpp"
-#define GLM_ENABLE_EXPERIMENTAL
-#include "glm/glm/gtx/quaternion.hpp"
-
-
-typedef glm::vec2 EVec2;
-typedef glm::vec3 EVec3;
-typedef glm::vec4 EVec4;
-typedef glm::vec4 EColor;
-typedef glm::mat4 EMat4;
-
 #include "file/engine_shared_buffer.h"
 #include "file/engine_folder.h"
 #include "file/engine_file.h"
