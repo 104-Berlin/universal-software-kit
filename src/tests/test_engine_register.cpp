1c2d3e50b3744e0d7a190461b37ad1a066bbd8f8
J4m3s00 <james.lol2000@gmail.com>
2021-04-15 10:03:42 +0200

    Update test_engine_register.cpp

--- src/tests/test_engine_register.cpp
+++ src/tests/test_engine_register.cpp
@@ -3,7 +3,7 @@
 #include <prefix_shared.h>
 
 using namespace Engine;
-
+using namespace entt::literals;
 struct MyTestComponent
 {
 	int SomeInteger;
@@ -19,4 +19,14 @@ TEST(RegisterTest, Basics)
 
 	EXPECT_TRUE(object1.HasComponent<MyTestComponent>());
 	EXPECT_EQ(object1.GetComponent<MyTestComponent>().SomeInteger, 32);
+
+	entt::meta<MyTestComponent>().type("MyTestComponent"_hs);
+	entt::meta<MyTestComponent>().data<&MyTestComponent::SomeInteger>("SomeInteger"_hs).prop("Name"_hs, "SomeInteger");
+
+
+
+	for (auto type : entt::resolve<MyTestComponent>().data())
+	{
+		E_INFO((char*) type.prop("Name"_hs).value().data());
+	}
 }
