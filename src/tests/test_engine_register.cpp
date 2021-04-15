a638ba75cae8ab0f9dbb3e4818f99b11ad1f7b1f
J4m3s00 <james.lol2000@gmail.com>
2021-04-15 22:29:55 +0200

    Add old event dispatcher back in

--- src/tests/test_engine_register.cpp
+++ src/tests/test_engine_register.cpp
@@ -19,14 +19,4 @@ TEST(RegisterTest, Basics)
 
 	EXPECT_TRUE(object1.HasComponent<MyTestComponent>());
 	EXPECT_EQ(object1.GetComponent<MyTestComponent>().SomeInteger, 32);
-
-	entt::meta<MyTestComponent>().type("MyTestComponent"_hs);
-	entt::meta<MyTestComponent>().data<&MyTestComponent::SomeInteger>("SomeInteger"_hs).prop("Name"_hs, "SomeInteger");
-
-
-
-	for (auto type : entt::resolve<MyTestComponent>().data())
-	{
-		E_INFO((char*) type.prop("Name"_hs).value().data());
-	}
 }
