41887e1d81ca4204f1dbad34e10abe5325e89a9c
J4m3s00 <james.lol2000@gmail.com>
2021-04-18 17:38:41 +0200

    Add some imguizmo testing

--- src/extension-examples/example1.cpp
+++ src/extension-examples/example1.cpp
@@ -14,6 +14,31 @@ std::vector<unsigned int> indices = {
 
 static Renderer::RMesh* mesh = nullptr;
 
+static float matrix[16] =
+{ 1.f, 0.f, 0.f, 0.f,
+    0.f, 1.f, 0.f, 0.f,
+    0.f, 0.f, 1.f, 0.f,
+    0.f, 0.f, 0.f, 1.f };
+
+
+static const float identityMatrix[16] =
+{ 1.f, 0.f, 0.f, 0.f,
+    0.f, 1.f, 0.f, 0.f,
+    0.f, 0.f, 1.f, 0.f,
+    0.f, 0.f, 0.f, 1.f };
+
+
+static float cameraView[16] =
+{ 1.f, 0.f, 0.f, 0.f,
+    0.f, 1.f, 0.f, 0.f,
+    0.f, 0.f, 1.f, 0.f,
+    0.f, 0.f, 0.f, 1.f };
+
+static float cameraProjection[16];
+
+static size_t selectedIndex = 0;
+static size_t selectedVertex = 0;
+
 class TestUiField : public Engine::EUIField
 {
 public:
@@ -31,9 +56,38 @@ public:
         size_t vertexId = 0;
         for (Renderer::RMesh::Vertex& vertex : fVertices)
         {
-            ImGui::PushID(vertexId++);
+            ImGui::PushID(vertexId);
             ImGui::InputFloat3("Position", &vertex.Position.x);
+            if (ImGui::Button("Select"))
+            {   
+                selectedVertex = vertexId;
+            }
+            if (selectedVertex == vertexId)
+            {
+                float matrixTranslation[3], matrixRotation[3], matrixScale[3];
+                matrixTranslation[0] = vertex.Position.x;
+                matrixTranslation[1] = vertex.Position.y;
+                matrixTranslation[2] = vertex.Position.z;
+
+                matrixRotation[0] = 0;
+                matrixRotation[1] = 0;
+                matrixRotation[2] = 0;
+
+                matrixScale[0] = 1;
+                matrixScale[1] = 1;
+                matrixScale[2] = 1;
+
+                //ImGuizmo::DecomposeMatrixToComponents(matrix, matrixTranslation, matrixRotation, matrixScale);
+                ImGuizmo::RecomposeMatrixFromComponents(&vertex.Position.x, matrixRotation, matrixScale, matrix);
+                ImGuizmo::Manipulate(cameraView, cameraProjection, ImGuizmo::OPERATION::TRANSLATE, ImGuizmo::MODE::LOCAL, matrix);
+
+                ImGuizmo::DecomposeMatrixToComponents(matrix, matrixTranslation, matrixRotation, matrixScale);
+                vertex.Position.x = matrixTranslation[0];
+                vertex.Position.y = matrixTranslation[1];
+                vertex.Position.z = matrixTranslation[2];
+            }
             ImGui::PopID();
+            vertexId++;
         }
         if (ImGui::Button("+##vertex"))
         {
@@ -58,8 +112,47 @@ public:
 
 static ERef<TestUiField> testUiField = nullptr;
 
+void OrthoGraphic(const float l, float r, float b, const float t, float zn, const float zf, float* m16)
+{
+   m16[0] = 2 / (r - l);
+   m16[1] = 0.0f;
+   m16[2] = 0.0f;
+   m16[3] = 0.0f;
+   m16[4] = 0.0f;
+   m16[5] = 2 / (t - b);
+   m16[6] = 0.0f;
+   m16[7] = 0.0f;
+   m16[8] = 0.0f;
+   m16[9] = 0.0f;
+   m16[10] = 1.0f / (zf - zn);
+   m16[11] = 0.0f;
+   m16[12] = (l + r) / (l - r);
+   m16[13] = (t + b) / (b - t);
+   m16[14] = zn / (zn - zf);
+   m16[15] = 1.0f;
+}
+
+
 void RenderViewport(Graphics::GContext* context, Graphics::GFrameBuffer* frameBuffer)
 {
+    ImGuiIO& io = ImGui::GetIO();
+    float viewHeight = 1.0f * io.DisplaySize.y / io.DisplaySize.x;
+    OrthoGraphic(-1.0f, 1.0f, 1.0, -1.0, 1000.f, -1000.f, cameraProjection);
+
+    float windowWidth = (float)ImGui::GetWindowWidth();
+    float windowHeight = (float)ImGui::GetWindowHeight();
+    ImGuizmo::SetRect(ImGui::GetWindowPos().x, ImGui::GetWindowPos().y, windowWidth, windowHeight);
+    float viewManipulateRight = ImGui::GetWindowPos().x + windowWidth;
+    float viewManipulateTop = ImGui::GetWindowPos().y;
+
+    ImGuizmo::SetDrawlist();
+
+    //ImGuizmo::DrawGrid(cameraView, cameraProjection, identityMatrix, 100.f);
+    //ImGuizmo::DrawCubes(cameraView, cameraProjection, &objectMatrix[0][0], 1);
+
+    //ImGuizmo::ViewManipulate(cameraView, 10, ImVec2(viewManipulateRight - 128, viewManipulateTop), ImVec2(128, 128), 0x10101010);
+
+
     mesh->SetData(testUiField->fVertices, testUiField->fIndices);
 
     Renderer::RRenderer3D renderer(context);
