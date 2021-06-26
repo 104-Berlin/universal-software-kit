9edcd016c8157bf734cc9f7ab7e609d90af1961d
J4m3s00 <james.lol2000@gmail.com>
2021-06-26 13:44:16 +0200

    Put the functions in cpp

---
+++ src/editor-extension/editor_vector_functions.cpp
@@ -0,0 +1,105 @@
+#include "editor_extension.h"
+
+template <>
+bool convert<Engine::EStructProperty, EVec2>::setter(Engine::EStructProperty* property, const EVec2& vec)
+{
+    using namespace Engine;
+	EValueProperty<double>* xProp = (EValueProperty<double>*)property->GetProperty("X");
+	EValueProperty<double>* yProp = (EValueProperty<double>*)property->GetProperty("Y");
+	if (xProp && yProp)
+	{
+		xProp->SetValue(vec.x);
+		yProp->SetValue(vec.y);
+		return true;
+	}
+	return false;
+}
+
+template <>
+bool convert<Engine::EStructProperty, EVec2>::getter(const Engine::EStructProperty* property, EVec2* outVec)
+{
+    using namespace Engine;
+	const EValueProperty<double>* xProp = (EValueProperty<double>*)property->GetProperty("X");
+	const EValueProperty<double>* yProp = (EValueProperty<double>*)property->GetProperty("Y");
+	if (xProp && yProp)
+	{
+		outVec->x = (float)xProp->GetValue();
+		outVec->y = (float)yProp->GetValue();
+		return true;
+	}
+	return false;
+}
+
+
+
+template <>
+bool convert<Engine::EStructProperty, EVec3>::setter(Engine::EStructProperty* property, const EVec3& vec)
+{
+    using namespace Engine;
+	EValueProperty<double>* xProp = (EValueProperty<double>*)property->GetProperty("X");
+	EValueProperty<double>* yProp = (EValueProperty<double>*)property->GetProperty("Y");
+	EValueProperty<double>* zProp = (EValueProperty<double>*)property->GetProperty("Z");
+	if (xProp && yProp && zProp)
+	{
+		xProp->SetValue(vec.x);
+		yProp->SetValue(vec.y);
+		zProp->SetValue(vec.z);
+		return true;
+	}
+	return false;
+}
+
+template <>
+bool convert<Engine::EStructProperty, EVec3>::getter(const Engine::EStructProperty* property, EVec3* outVec)
+{
+    using namespace Engine;
+	const EValueProperty<double>* xProp = (EValueProperty<double>*)property->GetProperty("X");
+	const EValueProperty<double>* yProp = (EValueProperty<double>*)property->GetProperty("Y");
+	const EValueProperty<double>* zProp = (EValueProperty<double>*)property->GetProperty("Z");
+	if (xProp && yProp && zProp)
+	{
+		outVec->x = (float)xProp->GetValue();
+		outVec->y = (float)yProp->GetValue();
+		outVec->z = (float)zProp->GetValue();
+		return true;
+	}
+	return false;
+}
+
+
+
+template <>
+bool convert<Engine::EStructProperty, EVec4>::setter(Engine::EStructProperty* property, const EVec4& vec)
+{
+    using namespace Engine;
+	EValueProperty<double>* xProp = (EValueProperty<double>*)property->GetProperty("X");
+	EValueProperty<double>* yProp = (EValueProperty<double>*)property->GetProperty("Y");
+	EValueProperty<double>* zProp = (EValueProperty<double>*)property->GetProperty("Z");
+	if (xProp && yProp && zProp)
+	{
+		xProp->SetValue(vec.x);
+		yProp->SetValue(vec.y);
+		zProp->SetValue(vec.z);
+		return true;
+	}
+	return false;
+}
+
+template <>
+bool convert<Engine::EStructProperty, EVec4>::getter(const Engine::EStructProperty* property, EVec4* outVec)
+{
+    using namespace Engine;
+	const EValueProperty<double>* xProp = (EValueProperty<double>*)property->GetProperty("X");
+	const EValueProperty<double>* yProp = (EValueProperty<double>*)property->GetProperty("Y");
+	const EValueProperty<double>* zProp = (EValueProperty<double>*)property->GetProperty("Z");
+	const EValueProperty<double>* wProp = (EValueProperty<double>*)property->GetProperty("W");
+	if (xProp && yProp && zProp && wProp)
+	{
+		outVec->x = (float)xProp->GetValue();
+		outVec->y = (float)yProp->GetValue();
+		outVec->z = (float)zProp->GetValue();
+		outVec->w = (float)wProp->GetValue();
+		return true;
+	}
+	return false;
+}
