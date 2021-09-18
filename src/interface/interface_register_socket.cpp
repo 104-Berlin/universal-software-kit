81855e00e361f42668de11ada4a6bafbca0ae205
J4m3s00 <james.lol2000@gmail.com>
2021-09-18 18:49:19 +0200

    init unix socket

--- src/interface/interface_register_socket.cpp
+++ src/interface/interface_register_socket.cpp
@@ -3,15 +3,45 @@
 using namespace Engine;
 
 ERegisterSocket::ERegisterSocket(int port) 
-    : fPort(port)
+    : fPort(port), fSocketId(-1)
 {
     
 }
 
 ERegisterSocket::~ERegisterSocket() 
 {
+    if (fSocketId > -1)
+    {
 #ifdef EWIN
-    
+
 #else
+        close(fSocketId);
 #endif
+    }
+}
+
+void ERegisterSocket::Init() 
+{
+    // TODO: For single machine interface use AF_LOCAL
+    int socketDomain = AF_INET;
+
+    fSocketId = socket(socketDomain, SOCK_STREAM, 0);
+
+    if (fSocketId == -1)
+    {
+        E_ERROR("Could not create socket!");
+        return;
+    }
+
+    fAddressInfo.sin_family = socketDomain;
+    fAddressInfo.sin_addr.s_addr = INADDR_ANY;
+    fAddressInfo.sin_port = htons(fPort);
+
+    if (bind(fSocketId, (const sockaddr*)&fAddressInfo, sizeof(fAddressInfo)) == -1)
+    {
+        E_ERROR("Could not bind the socket " + std::to_string(fSocketId));
+        return;
+    }
+
+    
 }
--- src/interface/interface_register_socket.h
+++ src/interface/interface_register_socket.h
@@ -6,12 +6,16 @@ namespace Engine {
     {
     private:
         ERegister       fLoadedRegister; // The register to get and set data
-
+        sockaddr_in     fAddressInfo; // Info to the current socket
+        
         int             fPort; // Running port
         int             fSocketId; // The running socket
     public:
         ERegisterSocket(int port);
         ~ERegisterSocket();
+
+    private:
+        void Init();
     };
 
 }
