404cb1e108160a5d0a2aae93b2d2e9742993a25f
J4m3s00 <james.lol2000@gmail.com>
2021-11-28 22:50:00 +0100

    ref

--- src/extension/tasks/extension_task.cpp
+++ src/extension/tasks/extension_task.cpp
@@ -2,12 +2,11 @@
 
 using namespace Engine;
 
-EBaseTask::EBaseTask(const EString& name, ETaskType type) 
+EBaseTask::EBaseTask(const EString& name, u16 type) 
     : fName(name), fType(type)
 {
     
 }
-
 
 EBaseTask::~EBaseTask() 
 {
@@ -17,4 +16,82 @@ EBaseTask::~EBaseTask()
 EStructProperty* EBaseTask::Execute(EStructProperty* inValue) 
 {
     return OnExecute(inValue);
+}
+
+
+ECFuncTask::ECFuncTask(const EString& name) 
+    : EBaseTask(name, ETaskType_SINGLE_EXECUTION)
+{
+    
+}
+
+ECFuncTask::ECFuncTask(const EString& name, CFunc_Param_Return func) 
+    : ECFuncTask(name)
+{
+    SetFunc(func);
+}
+
+ECFuncTask::ECFuncTask(const EString& name, CFunc_Param_NoReturn func) 
+    : ECFuncTask(name)
+{
+    SetFunc(func);   
+}
+
+ECFuncTask::ECFuncTask(const EString& name, CFunc_NoParam_Return func) 
+    : ECFuncTask(name)
+{
+    SetFunc(func);
+}
+
+ECFuncTask::ECFuncTask(const EString& name, CFunc_NoParam_NoReturn func) 
+    : ECFuncTask(name)
+{
+    SetFunc(func);
+}
+
+ECFuncTask::~ECFuncTask() 
+{
+    
+}
+
+EStructProperty* ECFuncTask::OnExecute(EStructProperty* inValue) 
+{
+    if (!fExecuteFunction)
+    {
+        E_ERROR("ECFuncTask::OnExecute: no function set");
+        return nullptr;
+    }
+
+    return fExecuteFunction(inValue);
+}
+
+void ECFuncTask::SetFunc(CFunc_NoParam_NoReturn func) 
+{
+    fExecuteFunction = [func](EStructProperty* inValue) -> EStructProperty*
+    {
+        func();
+        return nullptr;
+    };
+}
+
+void ECFuncTask::SetFunc(CFunc_NoParam_Return func) 
+{
+    fExecuteFunction = [func](EStructProperty* inValue) -> EStructProperty*
+    {
+        return func();
+    };
+}
+
+void ECFuncTask::SetFunc(CFunc_Param_NoReturn func) 
+{
+    fExecuteFunction = [func](EStructProperty* inValue) -> EStructProperty*
+    {
+        func(inValue);
+        return nullptr;
+    };
+}
+
+void ECFuncTask::SetFunc(CFunc_Param_Return func) 
+{
+    fExecuteFunction = func;
 }
--- src/extension/tasks/extension_task.h
+++ src/extension/tasks/extension_task.h
@@ -2,11 +2,10 @@
 
 namespace Engine {
 
-    E_STORAGE_ENUM(ETaskType,
-        SINGLE_EXECUTION,
-        ALLWAYS_RUNNING,
-        EVENT_DRIVEN
-    );
+    static constexpr u16 ETaskType_TASK_NONE = 0;
+    static constexpr u16 ETaskType_SINGLE_EXECUTION = BIT(1);
+    static constexpr u16 ETaskType_ALLWAYS_RUNNING = BIT(2);
+    static constexpr u16 ETaskType_EVENT_DRIVEN = BIT(3);
 
 
     class EBaseTask
@@ -14,13 +13,38 @@ namespace Engine {
     private:
         EString fName;
 
-        ETaskType fType;
+        u16 fType;
     public:
-        EBaseTask(const EString& name, ETaskType type);
+        EBaseTask(const EString& name, u16 type);
         ~EBaseTask();
 
         EStructProperty* Execute(EStructProperty* inValue);
         virtual EStructProperty* OnExecute(EStructProperty* inValue) = 0;
+    };
+
+    class ECFuncTask : public EBaseTask
+    {
+    private:
+        using CFunc_NoParam_NoReturn = std::function<void()>;
+        using CFunc_NoParam_Return = std::function<EStructProperty*()>;
+        using CFunc_Param_NoReturn = std::function<void(EStructProperty*)>;
+        using CFunc_Param_Return = std::function<EStructProperty*(EStructProperty*)>;
+
+        CFunc_Param_Return fExecuteFunction;
+    public:
+        ECFuncTask(const EString& name);
+        ECFuncTask(const EString& name, CFunc_Param_Return func);
+        ECFuncTask(const EString& name, CFunc_Param_NoReturn func);
+        ECFuncTask(const EString& name, CFunc_NoParam_Return func);
+        ECFuncTask(const EString& name, CFunc_NoParam_NoReturn func);
+        ~ECFuncTask();
+
+        EStructProperty* OnExecute(EStructProperty* inValue);
+
+        void SetFunc(CFunc_NoParam_NoReturn func);
+        void SetFunc(CFunc_NoParam_Return func);
+        void SetFunc(CFunc_Param_NoReturn func);
+        void SetFunc(CFunc_Param_Return func);
     };
 
 }
